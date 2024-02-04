cef2155d1477fc567dd30755cc7b106b92d0842d
mustafa <mustafa.piplod@gmail.com>
2024-02-04 22:46:30 +0530

    some chnages and addon added

---
+++ my_addons/real_estates_ads/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Real Estates Ads",
+    "version": "1.0",
+    "author": "Mustafa",
+    "category": "Sales",
+    "description": "Real Estate",
+    "data": [],
+    "depends": [],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
